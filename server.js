1cc0f46f9839cdfd6df2d00bfbee0ebb3951e5be
Guangting Wang <guwgoo@gmail.com>
2018-05-12 15:56:55 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const es = require('./routes/empservice');
+
+app.set('port',4000);
+app.use(express,static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use('/rest/es',es);
+
+app.listen(app.get('port'),()=>{
+    console.log('server')
+});
